e9a07fa4aad2025a4235cf4c0bf35599c3d7461e
MitsukiN <zhongweiguangxy@gmai.com>
2022-01-24 08:33:13 +0900

    my second commit

---
+++ cubed.py
@@ -0,0 +1,56 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#1
+nums=[1,2,3,4,5,]
+statistics.median_low(nums)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    statistics.median_low(nums)
+NameError: name 'statistics' is not defined
+nums=[1,2,3,4,5,]
+statistics.mean.(nums)
+SyntaxError: invalid syntax
+statistics.mean(nums)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    statistics.mean(nums)
+NameError: name 'statistics' is not defined
+import statistics.
+SyntaxError: invalid syntax
+import statistics
+nums=[1,2,3,4,5]
+statistics.median_high(nums)
+3
+statistics.median_low(nums)
+3
+#2
+import cubed
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    import cubed
+ModuleNotFoundError: No module named 'cubed'
+#2
+def cube_it(x):
+    return x**3
+import cubed
+SyntaxError: invalid syntax
+import cube
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    import cube
+ModuleNotFoundError: No module named 'cube'
+import cubed
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    import cubed
+ModuleNotFoundError: No module named 'cubed'
+result=cubed_it(3)
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    result=cubed_it(3)
+NameError: name 'cubed_it' is not defined
+print(result)
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    print(result)
+NameError: name 'result' is not defined
